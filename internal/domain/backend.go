f2f225a414b048aafb60079ba8bda7319603dd50
Rubén Martín <ruben.martin@afronta.com>
2026-04-20 17:22:03 +0200

    🏗️ Remove DetectBackend
    Remove the DetectBackend (it caused a circular dependency)

--- internal/domain/backend.go
+++ internal/domain/backend.go
@@ -1,7 +1,5 @@
 package domain
 
-import "fmt"
-
 // Backend defines the interface for setting wallpapers.
 type Backend interface {
 	IsAvaliable() bool
@@ -9,9 +7,4 @@ type Backend interface {
 	GetCurrentWallpaper() (string, error)
 	SupportedModes() []WallpaperMode
 	Name() string
-}
-
-// DetectBackend detects and returns the appropriate wallpaper backend.
-func DetectBackend() (Backend, error) {
-	return nil, fmt.Errorf("no backend detected")
 }
